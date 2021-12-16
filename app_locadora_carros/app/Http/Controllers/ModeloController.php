65fb061167f91902a7f42191b104350c0d4fd1b9
nathan <nathangar01@gmail.com>
2021-12-15 22:12:20 -0300

    Resource para Modelo

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ModeloController extends Controller
 {
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +19,7 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json($this->modelo->all(), 200);
     }
 
     /**
@@ -25,7 +30,22 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+
+        $image = $request->imagem;
+        $imagem_urn = $image->store('imagens/modelos', 'public');
+
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return $modelo;
     }
 
     /**
@@ -34,31 +54,89 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) return response()->json(['erro' => 'Recurso pesquisado nao existe'], 404);
+
+        return $modelo;
     }
+
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function findByName($name)
+    {
+        $modelo = $this->modelo->where('nome', 'like', '%'.$name.'%')->get();
+        return $modelo;
+    }
+
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Modelo  $modelo
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) return response()->json(['erro' => 'Recurso pesquisado nao existe'], 404);
+
+
+        if($request->method() === 'PATCH'){
+
+            $regrasDinamicas = array();
+            foreach($modelo->rules() as $input => $regra){
+                if(array_key_exists($input,$request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+        }else{
+            $request->validate($modelo->rules());
+        }
+
+        if($request->imagem) Storage::disk('public/modelos')->delete($modelo->imagem);
+
+        $image = $request->imagem;
+        $imagem_urn = $image->store('imagens/modelos', 'public');
+
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return $modelo;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Modelo  $modelo
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) return response()->json(['erro' => 'Recurso pesquisado nao existe'], 404);
+
+        if($modelo->imagem) Storage::disk('public/modelos')->delete($modelo->imagem);
+
+        $modelo->delete();
+        return ['msg' => 'O modelo foi removido com sucesso!'];
     }
 }
